328732dbdd09b393ef24bb98d685d784af266cea
Gabriel <gabriel.kpaz@gmail.com>
2025-07-18 21:41:45 -0300

    minnor changes

--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -15,7 +15,7 @@ function Actions() {
 
     setLoading(true);
     try {
-      const response = await axios.post('http://localhost:3001/generate-resume', { layout, resumeData, themeColor }, { responseType: 'blob' });
+      const response = await axios.post('https://gerador-curriculos-backend.onrender.com', { layout, resumeData, themeColor }, { responseType: 'blob' });
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement('a');
       link.href = url;
